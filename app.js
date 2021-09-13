e1d8ce200d9099e7a5e66bfaa7f9a9b0e7e42ac9
Chinh <do.inby@gmail.com>
2021-09-14 08:43:58 +1000

    create grid

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const $grid = document.querySelector('.grid');
 const $startButton = document.querySelector('#start');
 const $score = document.querySelector('#score');
+
+let squares = [];
+
+function createGrid() {
+    for(let i = 0; i < 100; i++) {
+        const $square = `<div class="square"></div>`;
+        squares = [...squares, $square];
+    }
+    $grid.innerHTML = squares.join('');    
+}
+
+createGrid();
