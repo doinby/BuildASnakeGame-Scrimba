3d7df52cfd50b602f59653159417c43576ed5b65
Chinh <do.inby@gmail.com>
2021-09-14 14:38:14 +1000

    added snake move function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const $startButton = document.querySelector('#start');
 const $score = document.querySelector('#score');
 
 let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
 
 function createGrid() {
     for(let i = 0; i < 100; i++) {
@@ -13,3 +15,22 @@ function createGrid() {
 }
 
 createGrid();
+
+// Draw snake at the top left
+const $squares = document.querySelectorAll('.grid .square');
+currentSnake.forEach(index => {
+    $squares[index].classList.add('snake');
+})
+
+function move() {
+    // Remove tail
+    const tail = currentSnake[currentSnake.length - 1];
+    $squares[tail].classList.remove('snake');
+
+    // Draw new head
+    currentSnake = currentSnake.map((snakeElement) => snakeElement + direction);
+    const head = currentSnake[0];
+    $squares[head].classList.add("snake");
+}
+
+move();
