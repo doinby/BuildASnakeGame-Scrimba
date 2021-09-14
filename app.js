5296d379b393703ba55ce0e4dfe9dfc57b53710c
Chinh <do.inby@gmail.com>
2021-09-15 09:26:11 +1000

    added logics to when snake hits walls

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const $grid = document.querySelector('.grid');
 const $startButton = document.querySelector('#start');
 const $score = document.querySelector('#score');
 
+const width = 10;
 let squares = [];
-let currentSnake = [2, 1, 0];
+let currentSnake = [11, 12, 13];
 let direction = 1;
-let width = 10;
 
 function createGrid() {
-    for(let i = 0; i < 100; i++) {
-        const $square = `<div class="square"></div>`;
+    for(let i = 0; i < width*width; i++) {
+        const $square = `<div class="square"><span>${i}</span></div>`;
         squares = [...squares, $square];
     }
     $grid.innerHTML = squares.join('');    
@@ -24,19 +24,27 @@ currentSnake.forEach(index => {
 })
 
 function move() {
-    //remove last element from our currentSnake array
+    if ((currentSnake[0] + width >= width * width && direction === width) || // if snake has hit bottom
+    (currentSnake[0] % width === width - 1 && direction === 1) || // if snake has hit right wall
+    (currentSnake[0] % width === 0 && direction === -1) || // if snake has hit left wall
+    (currentSnake[0] - width < 0 && direction === -width) || // if snake has hit top
+    $squares[currentSnake[0] + direction].classList.contains('snake')) // hits itself
+    return clearInterval(timerId);
+    
+    // remove last element from our currentSnake array
     const tail = currentSnake.pop();
-    //remove styling from last element
+    // remove styling from last element
     $squares[tail].classList.remove("snake");
-    //add square in direction we are heading
+    // add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction);
-    //add styling so we can see it
+    // add styling so we can see it
     $squares[currentSnake[0]].classList.add("snake");
 }
 
+move();
 let timeId = setInterval(move, 1000);
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keyup', event => {
     switch (event.code) {
         case "ArrowDown":
         direction = width;
